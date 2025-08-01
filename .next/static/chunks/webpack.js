47e60579c1391f1a30a3fa99a4e3522b9fdb710e
careernestteam <careernest.team@gmail.com>
2025-08-01 10:51:46 +0100

    Fix missing Supabase environment variables

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d84b1deee179b41b"; }
+/******/ 		__webpack_require__.h = function() { return "ad403ffe6c777270"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
